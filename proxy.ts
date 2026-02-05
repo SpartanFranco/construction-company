a12ec423406a1fb8d03e8e7743c079c1389695f5
Geovani Franco <geovanispartaco@gmail.com>
2026-02-04 20:59:25 -0500

    fix: resolve redirect loop and active styles in production

--- proxy.ts
+++ proxy.ts
@@ -6,11 +6,11 @@ export default async function proxy(request: NextRequest) {
 	const { pathname } = request.nextUrl;
 
 	if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-		const sessionCookies = request.cookies.get(
-			'better-auth.session_token',
-		)?.value;
+		const sessionToken =
+			request.cookies.get('better-auth.session_token')?.value ||
+			request.cookies.get('__Secure-better-auth.session_token')?.value;
 
-		if (!sessionCookies) {
+		if (!sessionToken) {
 			return NextResponse.redirect(new URL('/login', request.url));
 		}
 	}
